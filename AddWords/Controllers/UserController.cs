6e421f7bd07ca8fae61b3805bec87de157b1726f
PedroHenrick1 <pedrohenrick52@gmail.com>
2025-03-01 12:18:42 -0300

    Crud Criado

--- AddWords/Controllers/UserController.cs
+++ AddWords/Controllers/UserController.cs
@@ -31,5 +31,57 @@ namespace AddWords.Controllers
 
             return CreatedAtAction(nameof(GetUsers), new { id = user.Id }, User);
         }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(int id, User user) 
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException) 
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(u => u.Id == id);
+        }
     }
 }
